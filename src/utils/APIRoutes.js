870226ed6996f5712fb50ffd2a3cfe3de994f748
mohitaiits <mohit.kadwe.cs19@ggits.net>
2022-07-17 01:28:21 +0530

    add backend link

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = process.env.REACT_APP_API_HOST;
+export const host = "https://chatting-go.herokuapp.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
